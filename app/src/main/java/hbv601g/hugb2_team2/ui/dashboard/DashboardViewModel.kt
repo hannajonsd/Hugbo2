2c1600328799fd4884883b9efb16d90fd8c6b6ee
bjoggi5 <bjorgvinhi@gmail.com>
2024-02-01 09:48:27 +0000

    empty project

---
+++ app/src/main/java/hbv601g/hugb2_team2/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package hbv601g.hugb2_team2.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
