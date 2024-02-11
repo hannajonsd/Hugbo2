e21a9a5c034549a58ff3ebf18e4884564e9d2a9f
bjoggi5 <bjorgvinhi@gmail.com>
2024-02-11 22:12:40 +0000

    establishment location enum

--- app/src/main/java/hbv601g/hugb2_team2/entities/Establishment.kt
+++ app/src/main/java/hbv601g/hugb2_team2/entities/Establishment.kt
@@ -8,10 +8,22 @@ data class Establishment(
     val type: String,
     val address: String,
     // area attribute = location attribute (which is an enum) in spring
-    val area: String,
+    val location: Location,
     val priceRating: Double,
     val selectionRating: Double,
     val userRating: Double,
-    val openingHours: String
-
-)
+    val openingHours: String,
+    val lat: Double,
+    val lon: Double,
+) {
+    enum class Location(val value: Int) {
+        Miðbærinn(0),
+        Höfuðborgarsvæðið(1),
+        Norðurland(2),
+        Suðurland(3),
+        Suðvesturland(4),
+        Austurland(5),
+        Vesturland(6),
+        Vestfirðir(7)
+    }
+}
