4bd5997d9a28b4adad30d3a0a2a6a0c8f1520f71
Mikael Andri Ingason <33830730+MikaelAndriIngason@users.noreply.github.com>
2024-02-25 20:54:54 +0000

    Byrjun á user processing
    Setja upp grunnin á að búa til user og skrá sig inn, einnig validation

--- app/src/main/java/hbv601g/hugb2_team2/ui/activities/user/CreateAccountActivity.kt
+++ app/src/main/java/hbv601g/hugb2_team2/ui/activities/user/CreateAccountActivity.kt
@@ -1,9 +1,16 @@
 package hbv601g.hugb2_team2.ui.activities.user
 
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import hbv601g.hugb2_team2.entities.User
 import hbv601g.hugb2_team2.R
 import hbv601g.hugb2_team2.services.providers.UserServiceProvider
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class CreateAccountActivity : AppCompatActivity() {
 
@@ -11,11 +18,65 @@ class CreateAccountActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
+
+        val nameInput: EditText = findViewById(R.id.signup_name)
+        val emailInput: EditText = findViewById(R.id.signup_email)
+        val passwordInput: EditText = findViewById(R.id.signup_pass)
+        val confirmPasswordInput: EditText = findViewById(R.id.signup_pass_confirm)
+        val submitButton: Button = findViewById(R.id.signup_btn)
+
+        val switchToLogin: Button = findViewById(R.id.switch_to_login_btn)
+
+        submitButton.setOnClickListener {
+            val name = nameInput.text.toString()
+            val email = emailInput.text.toString()
+            val password = passwordInput.text.toString()
+            val confirmPassword = confirmPasswordInput.text.toString()
+
+            if (name.isEmpty()) {
+                nameInput.error = "Username can't be blank"
+                return@setOnClickListener
+            }
+
+            if (email.isEmpty()) {
+                emailInput.error = "Email can't be blank"
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()) {
+                passwordInput.error = "Password can't be blank"
+                return@setOnClickListener
+            }
+
+            if (confirmPassword.isEmpty()) {
+                confirmPasswordInput.error = "Password can't be blank"
+                return@setOnClickListener
+            }
+
+            if (password != confirmPassword) {
+                confirmPasswordInput.error = "Passwords do not match"
+                return@setOnClickListener
+            }
+
+            // TODO: Salt-a password
+
+            // POST-a user í database
+            val newUser = User(0, name, password, name, name, email,false)
+            CoroutineScope(Dispatchers.IO).launch {
+                userService.createUser(newUser)
+            }
+
+            // Ef allt er í lagi, þá skipta yfir í login activity
+            goToLoginActivity()
+        }
+
+        switchToLogin.setOnClickListener {
+            goToLoginActivity()
+        }
     }
 
-    /* val btnSwitchToLogin = findViewById<Button>(R.id.switch_to_login_btn)
-    btnSwitchToLogin.setOnClickListener {
+    private fun goToLoginActivity() {
         val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
-    } */
+    }
 }
--- app/src/main/java/hbv601g/hugb2_team2/ui/activities/user/LoginActivity.kt
+++ app/src/main/java/hbv601g/hugb2_team2/ui/activities/user/LoginActivity.kt
@@ -1,9 +1,15 @@
 package hbv601g.hugb2_team2.ui.activities.user
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import hbv601g.hugb2_team2.R
 import hbv601g.hugb2_team2.services.providers.UserServiceProvider
+import android.widget.EditText
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class LoginActivity : AppCompatActivity() {
 
@@ -11,5 +17,41 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val usernameInput: EditText = findViewById(R.id.login_username)
+        val passwordInput: EditText = findViewById(R.id.login_pass)
+        val submitButton: Button = findViewById(R.id.login_btn)
+
+        val switchToCreateAccount: Button = findViewById(R.id.switch_to_create_btn)
+
+        submitButton.setOnClickListener {
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+
+            if (username.isEmpty()) {
+                usernameInput.error = "Email can't be blank"
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()) {
+                passwordInput.error = "Password can't be blank"
+                return@setOnClickListener
+            }
+
+            CoroutineScope(Dispatchers.IO).launch {
+                val user = userService.getUserByUsername(username)
+
+                if (user == null || user.password == password) {
+                    // TODO: Skila error message
+                }
+
+                // TODO: Ef allt er í lagi, þá skipta yfir í profile eða main menu
+            }
+        }
+
+        switchToCreateAccount.setOnClickListener {
+            val intent = Intent(this, CreateAccountActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
