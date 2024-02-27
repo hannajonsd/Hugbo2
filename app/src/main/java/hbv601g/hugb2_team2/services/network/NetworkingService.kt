940e841bdff9f35556c518e79d19d1ed73438b3f
bjoggi5 <bjorgvinhi@gmail.com>
2024-02-27 19:00:27 +0000

    Hægt að búa til establishment með tengingu við bakenda! Notandi þarf að vera skráður inn og admin til að komast á create establishment activity.

--- app/src/main/java/hbv601g/hugb2_team2/services/network/NetworkingService.kt
+++ app/src/main/java/hbv601g/hugb2_team2/services/network/NetworkingService.kt
@@ -1,13 +1,12 @@
 package hbv601g.hugb2_team2.services.network
 
-import android.content.Context
 import com.google.gson.JsonElement
 import org.json.JSONObject
 
 interface NetworkingService {
 
     suspend fun getRequest(reqURL: String): JsonElement
-    suspend fun postRequest(reqURL: String, data: JSONObject): JSONObject
+    suspend fun postRequest(reqURL: String, data: String): JSONObject
     suspend fun putRequest(reqURL: String, data: JSONObject): JSONObject
     suspend fun patchRequest(reqURL: String, data: JSONObject): JSONObject
     suspend fun deleteRequest(reqURL: String): JSONObject
--- app/src/main/java/hbv601g/hugb2_team2/services/network/NetworkingServiceImpl.kt
+++ app/src/main/java/hbv601g/hugb2_team2/services/network/NetworkingServiceImpl.kt
@@ -3,9 +3,9 @@ package hbv601g.hugb2_team2.services.network
 import android.content.Context
 import com.android.volley.Request
 import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.google.gson.JsonElement
-import hbv601g.hugb2_team2.WineNotApp
 import kotlinx.coroutines.suspendCancellableCoroutine
 import org.json.JSONObject;
 import kotlin.coroutines.resume
@@ -17,25 +17,39 @@ class NetworkingServiceImpl(applicationContext: Context) : NetworkingService {
     private var BASE_URL = "http://10.0.2.2:8080/api"
 
     private val context = applicationContext
-    var apiRequestQueue : RequestQueue = Volley.newRequestQueue(context)
+    var apiRequestQueue: RequestQueue = Volley.newRequestQueue(context)
 
-    override suspend fun getRequest(reqURL: String): JsonElement = suspendCancellableCoroutine { continuation ->
-    val url = "$BASE_URL$reqURL"
-    val jsonElementRequest = JsonElementRequest(
-        Request.Method.GET, url,
-        { response ->
-            continuation.resume(response)
-        },
-        { error ->
-            continuation.resumeWithException(Exception(error.toString()))
+    override suspend fun getRequest(reqURL: String): JsonElement =
+        suspendCancellableCoroutine { continuation ->
+            val url = "$BASE_URL$reqURL"
+            val jsonElementRequest = JsonElementRequest(
+                Request.Method.GET, url,
+                { response ->
+                    continuation.resume(response)
+                },
+                { error ->
+                    continuation.resumeWithException(Exception(error.toString()))
+                }
+            )
+            apiRequestQueue.add(jsonElementRequest)
         }
-    )
-    apiRequestQueue.add(jsonElementRequest)
-}
 
-    override suspend fun postRequest(reqURL: String, data: JSONObject): JSONObject {
-        TODO("Not yet implemented")
+    override suspend fun postRequest(reqURL: String, data: String): JSONObject =
+        suspendCancellableCoroutine { continuation ->
+            val url = "$BASE_URL$reqURL"
+            val jsonObject = JSONObject(data)
+            val jsonObjectRequest = JsonObjectRequest(
+                Request.Method.POST, url, jsonObject,
+                { response ->
+                    continuation.resume(response)
+                },
+                { error ->
+                    continuation.resumeWithException(Exception(error.toString()))
+                }
+            )
+            apiRequestQueue.add(jsonObjectRequest)
     }
+
 
     override suspend fun putRequest(reqURL: String, data: JSONObject): JSONObject {
         TODO("Not yet implemented")
