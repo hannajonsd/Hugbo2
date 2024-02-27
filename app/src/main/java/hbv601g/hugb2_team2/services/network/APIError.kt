cec0baa5c48ff5992b317b6429949b18ca3fc483
bjoggi5 <bjorgvinhi@gmail.com>
2024-02-27 18:23:51 +0000

    tók út óþarfi klasa

--- app/src/main/java/hbv601g/hugb2_team2/services/network/APIError.kt
+++
@@ -1,7 +0,0 @@
-package hbv601g.hugb2_team2.services.network
-
-class APIError(
-    val error: String,
-    val response: String
-) {
-}
--- app/src/main/java/hbv601g/hugb2_team2/services/network/APIResponse.kt
+++
@@ -1,7 +0,0 @@
-package hbv601g.hugb2_team2.services.network
-
-class APIResponse(
-    val error: String,
-    val response: String
-) {
-}
